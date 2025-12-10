90881fc8ea3c38aba3d0eafc7c0bc0d8335238fb
Jacob <Jacques.kanu@gmail.com>
2025-12-10 22:19:27 +0100

    formatDate and formatTime utils functions

--- apps/frontend/src/lib/utils.ts
+++ apps/frontend/src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(dateString: string) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+}
+
+export function formatTime(timeString: string) {
+  const [hours, minutes] = timeString.split(":");
+  return `${hours}:${minutes}`;
+}
